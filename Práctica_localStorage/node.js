0f0ab5d0e125786b65f9cbeb97789797429979c4
DaniJimz <danielji55@gmail.com>
2026-01-07 14:05:39 +0100

    Práctica localstorage

---
+++ Práctica_localStorage/node.js
@@ -0,0 +1,7 @@
+const juego = document.getElementById('juego');
+const añadir = document.getElementById('añadir');
+const mostrar = document.getElementById('mostrar');
+const eliminar = document.getElementById('eliminar');
+const lista = document.getElementById('lista');
+let juegos = [];
+
