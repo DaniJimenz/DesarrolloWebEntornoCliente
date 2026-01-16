d1f531dfefc73d282eb92223cfd778f8fe1df5e5
DaniJimz <danielji55@gmail.com>
2026-01-16 10:18:22 +0100

    Tablero Kanban

---
+++ Práctica_localStorage/main.js
@@ -0,0 +1,13 @@
+const botonA = document.getElementById('botonA');
+const botonB = document.getElementById('botonB');
+const columna = document.getElementById('columna');
+const nombre = document.getElementById('nombre');
+const limpiar = document.getElementById('limpiar');
+const label = document.getElementById('label');
+const formulario = document.getElementById('formulario');
+const 
+
+
+
+
+
